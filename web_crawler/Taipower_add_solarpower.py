ec4907a01ff92ab485e2fc23bd824719ab59471f
pat <pat@example.com>
2021-03-01 21:05:57 +0800

    Update - Taipower_add_solarpower
    Use learned skill from datacamp course.

--- web_crawler/Taipower_add_solarpower.py
+++ web_crawler/Taipower_add_solarpower.py
@@ -2,46 +2,32 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 # data from https://www.taipower.com.tw/d006/loadGraph/loadGraph/genshx_.html
-# analysis html get url
+# analysis html construct get url
 url = "https://www.taipower.com.tw/d006/loadGraph/loadGraph/data/genary.txt"
 page = requests.get(url)
-page.encoding='utf-8'
-soup = BeautifulSoup(page.text,'lxml')
+page.encoding = 'utf-8'
+soup = BeautifulSoup(page.text, 'lxml')
 # get text
 text = soup.get_text()
 text_list = text.split('[')
-L = len(text_list)
-
-solar_p=[]
-for i in range(1,L):
-    if '太陽能' in text_list[i]:
-        if '小計' not in text_list[i]:
-            if '太陽能購電' not in text_list[i]:
-             solar_p =solar_p + [i]
-solar =[]
-solar_index=[]
-for i in solar_p:
-    s = text_list[i]
-    s = s.rstrip(', "],')
-    s = s.split(',')
-    for j in range(0,len(s)):
-        s[j] = s[j].strip(' " ')
-        s[j] = s[j].rstrip(' " ')
-
-    solar = solar + [[s[3]]]
-    solar_index = solar_index + [s[1]]
-time =text_list[0]
-str = ''
-time = str.join(list(filter(lambda ch: ch in '0123456789-: ', time)))
-time = time.strip(':')
-time = [time.rstrip(':')]
-#time = [time.rstrip(':')]
-my_df =pd.read_csv('data\\solar.csv',encoding='utf-8',index_col=0)
-solar = pd.DataFrame(solar,index=solar_index)
-time  = pd.DataFrame(time,index=['時間'])
-solar = time.append(solar)
-solar = solar.T
-my_df = my_df.append(solar,ignore_index = True)
-my_df.to_csv('data\\solar.csv',encoding='utf-8-sig')
+# clean useless value and get solar power.
+solar_p = [i for i in text_list[1:] if ('太陽能' in i) & ('小計' not in i) & ('太陽能購電' not in i)]
+solar = []
+solar_index = []
+solar_temp = [i.rstrip(', "],').split(',') for i in solar_p]
+solar_dict = {i[1].strip(' " ').rstrip(' " '): [float(i[3].strip(' " ').rstrip(' " '))] for i in solar_temp}
+# get time
+time = text_list[0]
+time_str = ''
+time = time_str.join(list(filter(lambda ch: ch in '0123456789-: ', time)))
+time = time.strip(':').rstrip(':')
+# save data
+my_df = pd.read_csv('data\\solar.csv', encoding='utf-8', index_col=0)
+solar = pd.DataFrame(solar_dict)
+solar['時間'] = time
+my_df.append(solar, ignore_index=True)
+first_col = my_df.pop('時間')  # set time as first col
+my_df.insert(0, '時間', first_col)
+my_df.to_csv('data\\solar.csv', encoding='utf-8-sig')
 
 
